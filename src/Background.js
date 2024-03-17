0a5500905fddc6289fd1e5531c3c3d6a7de8b24c
Max Lee <m.m.lee@student.saintkentigern.com>
2024-03-18 12:26:42 +1300

    adding glassmprphism to nav

---
+++ src/Background.js
@@ -0,0 +1,6 @@
+export default function Background() {
+    return (
+        <div className="h-72 bg-gradient-to-r from-purple-600 to-blue-600">
+
+        </div>
+    );}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Nav from './Nav';
+import Background from './Background';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Nav />
+    <Background />
   </React.StrictMode>
 );
 
